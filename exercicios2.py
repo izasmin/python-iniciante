9e441f5b3aef4fbf5c49fcdc202e621896f69025
izasmin <67179044+izasmin@users.noreply.github.com>
2022-09-25 17:52:21 -0400

    Add files via upload

---
+++ exercicios2.py
@@ -0,0 +1,13 @@
+#Aprovado ou Reprovado
+nota=int(input("Digite para saber se foi aprovado ou reprovado: "))
+if nota<4:
+    print("Reprovado")
+elif nota >4 and nota<=6:
+    print("Exame")
+else:
+    print("Aprovado")
+#Escreva uma expressão que será utilizada para decidir se um aluno foi ou não aprovado.
+# Para ser aprovado, todas as médias do aluno devem ser maiores que 7. Considere que o aluno cursa apenas três matérias,
+# e que a nota de cada uma está armazenada nas seguintes variáveis: matéria1, matéria2 e matéria3.
+nota1 = int(input(" Sua nota: "))
+nota2 = int(input(" Sua nota: "))
