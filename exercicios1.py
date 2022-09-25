978547f1f830b86c75a8f9ae21b98ed97462afcf
izasmin <67179044+izasmin@users.noreply.github.com>
2022-09-24 23:00:39 -0400

    Add files via upload

---
+++ exercicios1.py
@@ -0,0 +1,13 @@
+#Faça um programa que leia 2 numeros e mostre a soma entre eles
+n1 = int(input('Num1: '))
+n2 = int(input('Num2: '))
+soma = n1+n2
+print(' A soma de {} e {} é {}'.format(n1,n2,soma))
+#Faça um programa que exiba seu nome na tela
+nome = input(' Seu nome: ')
+print('Seu nome é {} Santos'.format(nome))
+#Calcule um aumento de 15% para um salário de R$750
+n1 = int(input('Aumento: %'))
+n2 = int(input('Salario: '))
+aumento = (n1/100)*n2 + n2
+print('Seu novo salario é {}'.format(aumento))
