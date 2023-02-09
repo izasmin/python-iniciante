46d8a313f4763b9b57d9c73baa9197be39b64797
izasmin <67179044+izasmin@users.noreply.github.com>
2023-02-08 23:23:01 -0400

    Create python-08-02-2023.py

---
+++ python-08-02-2023.py
@@ -0,0 +1,36 @@
+https://ilumeo.com.br/todos-posts/2021/03/30/o-uso-de-dados-na-industria-da-moda
+https://algorithms-tour.stitchfix.com/
+https://gummy.digital/conteudo-de-moda/
+  
+A Stitch Fix foi fundada em 2011, em São Francisco, na Califórnia. 
+Ela presta um serviço de estilo pessoal a homens e mulheres. 
+Ao criar um perfil no site, o usuário responde a perguntas como: “você gosta de fazer compras?” e “quanto tempo você gasta para se preparar de manhã?”. 
+Com o resultado, há uma experiência personalizada de recomendações de looks para vestir.
+
+Existem várias tecnologias diferentes para tornar essa ferramenta bem-sucedida, como sistemas de recomendação, computação humana 
+(aproveitando a inteligência humana para resolver problemas computacionais), 
+otimização de logística, máquinas de estado (uma máquina pode ter diferentes estados, mas apenas cumprir um de cada vez), modelagem de demanda, 
+gerenciamento de estoque e uma plataforma de dados (banco de dados do cliente acessível a outros sistemas). 
+
+
+Stitch Fix pode ser conceituado como um serviço de assinatura de estilo online, 
+eliminando a necessidade de seus clientes saírem e comprar roupas ou até mesmo navegar online, 
+porque recebem recomendações personalizadas na hora certa, em uma programação regular. 
+Os clientes podem ficar com todas as peças de roupas que compram ou devolver o que não gostam ou não precisam. 
+Esse feedback é inserido na estrutura de dados da empresa para tornar os algoritmos ainda melhores na determinação do estilo preferido para cada pessoa e até mesmo 
+identificar tendências.
+
+Em entrevista,quando um cliente se inscreve no serviço, são mais de 90 pontos de dados diferentes.
+
+ao lançar uma nova coleção, muitas marcas postam fotos nas redes sociais para coletar feedback e monitorar o consenso do público. 
+Elas usam esses comentários para fazer as alterações necessárias antes de seu lançamento oficial. Todo esse processo é conhecido como análise de sentimento, 
+em que as informações publicamente disponíveis podem ser convertidas em dados estruturados e utilizáveis.
+
+Outra área em que a internet tem ajudado significativamente os varejistas de moda é no layout de suas lojas de rua. Por meio do serviço wi-fi gratuito da loja, 
+os cientistas de dados podem rastrear e usar a conexão de cada cliente para determinar uma série de coisas. Eles podem dizer, por exemplo, quanto tempo os clientes 
+passam na loja, com que frequência voltam e em quais seções passam a maior parte do tempo. Usando essas informações,
+é possível informar os gerentes sobre como moldar o layout da loja, para que os itens comprados com frequência possam ser colocados em posições ideais.
+
+
+https://github.com/adrianosantospb/RFV
+  https://github.com/adrianosantospb/RFV/blob/master/RFM%20-%20Analysis.ipynb
